c457f655572bf4d3452df10d3334f7be7e52b84a
Dawood Mohammed <dawood.mohammed@appnetwise.com>
2022-08-31 16:23:31 +0530

    first commit

---
+++ src/main/java/com/adaequare/interview/challenges/MapEqualsChallenge.java
@@ -0,0 +1,36 @@
+package com.adaequare.interview.challenges;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapEqualsChallenge {
+    public static void main(String... doYourBest) {
+        Map<Stark, String> map = new LinkedHashMap<>();
+        map.put(new Stark("Arya"), "1");  // {"xyz" : "1"}
+        map.put(new Stark("Ned"), "2");
+        map.put(new Stark("Sansa"), "3");
+        map.put(new Stark("Bran"), "4");
+        map.put(new Stark("Jaime"), "5");
+        map.forEach((k, v) -> System.out.print(v));
+    }
+
+    static class Stark {
+        String name;
+        Stark(String name) {
+            this.name = name;
+        }
+
+        public boolean equals(Object obj) {
+            return ((Stark)obj).name.length() == this.name.length();
+        }
+
+        public int hashCode() {
+            return 4000 << 2 * 2000 / 10000;
+        }
+    }
+
+    //A) 245
+    //B) 123
+    //C) 425
+    //D) 12345
+}
